ce8fa26783d75a2c2961525b2755e41f328deeb6
Koneweekk <rlagkswn37@naver.com>
2024-03-25 17:30:38 +0900

    [Feat]-Hanju 1.Remove reservation when delete user info

--- room_reservation/src/room_reservation/UserInputerLoginPW.java
+++ room_reservation/src/room_reservation/UserInputerLoginPW.java
@@ -12,7 +12,7 @@ public class UserInputerLoginPW extends UserInputer{
   public boolean validator(String[] input) {
     boolean isValid = false;
 		if (!input[0].equals(input[1])) {
-			System.out.println("P비밀번호가 일치하지 않습니다.");
+			System.out.println("비밀번호가 일치하지 않습니다.");
 		} else isValid = true;
 
 		return isValid;
